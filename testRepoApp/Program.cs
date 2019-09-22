e60e558fd8dc535d05e8e180adb3cf44873a2eef
ullanor <pawel.nowacki95@gmail.com>
2019-09-22 15:25:10 +0200

    Issue1 fulfilled and added multiplication

--- testRepoApp/Program.cs
+++ testRepoApp/Program.cs
@@ -15,6 +15,7 @@ namespace testRepoApp
                 Console.WriteLine("0 - Exit");
                 Console.WriteLine("1 - Addition");
                 Console.WriteLine("2 - Substraction");
+                Console.WriteLine("3 - Multiplication");
                 int input = int.Parse(Console.ReadLine());
                 if (input == 0)
                     break;
