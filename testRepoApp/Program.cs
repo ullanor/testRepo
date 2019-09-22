537c74f0118e4bf59d68cd5a583bac24ddf09275
ullanor <pawel.nowacki95@gmail.com>
2019-09-22 15:08:44 +0200

    Add exit to Main

---
+++ testRepoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testRepoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0 - exit");
+                int input = int.Parse(Console.ReadLine());
+                if (input == 0)
+                    break;
+
+            }
+        }
+    }
+}
