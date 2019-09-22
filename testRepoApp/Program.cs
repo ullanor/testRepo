b1b1736800383049d3b52dd8db40ef42a7a8365d
ullanor <pawel.nowacki95@gmail.com>
2019-09-22 15:44:28 +0200

    Issue done!

--- testRepoApp/Program.cs
+++ testRepoApp/Program.cs
@@ -16,6 +16,7 @@ namespace testRepoApp
                 Console.WriteLine("1 - Addition");
                 Console.WriteLine("2 - Substraction");
                 Console.WriteLine("3 - Multiplication");
+                Console.WriteLine("4 - Power");
                 int input = int.Parse(Console.ReadLine());
                 if (input == 0)
                     break;
