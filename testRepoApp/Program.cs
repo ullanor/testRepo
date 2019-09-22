b56d0440ec6a799883b8b6a5458a13128ea3a869
ullanor <pawel.nowacki95@gmail.com>
2019-09-22 16:57:44 +0200

    super duper update

--- testRepoApp/Program.cs
+++ testRepoApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,12 +18,59 @@ namespace testRepoApp
                 Console.WriteLine("1 - Addition");
                 Console.WriteLine("2 - Substraction");
                 Console.WriteLine("3 - Multiplication");
-                Console.WriteLine("4 - Power");
-                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine("55 - Download txt file");
+                Console.WriteLine("56 - Read text from downl.file");
+                Console.WriteLine("57 - Remove downloaded file");
+                int input = 99;
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
                 if (input == 0)
                     break;
-
+                if (input == 55)
+                    DownloadFileFromWeb();
+                if (input == 56)
+                    ReadDownloadedFile();
+                if (input == 57)
+                    RemoveDownloadedFile();
             }
+
+        }
+
+        static void RemoveDownloadedFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//webText.txt";
+            if (!File.Exists(path))
+                return;
+            File.Delete(path);
+            Console.WriteLine("File was deleted!");
+        }
+
+        static void ReadDownloadedFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//webText.txt";
+            if (!File.Exists(path))
+                return;
+            string fileString = File.ReadAllText(path);
+            Console.WriteLine("\n"+fileString);
+        }
+
+        static void DownloadFileFromWeb()
+        {
+            Console.WriteLine("Download request processing!");
+            string remoteUri = "https://s3.zylowski.net/public/input/1.txt";
+
+            WebClient myWebClient = new WebClient();
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", "1.txt", 
+                remoteUri.Substring(0,remoteUri.Length-5));
+
+            myWebClient.DownloadFile(remoteUri, Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//webText.txt");
+            Console.WriteLine("Successfully Downloaded File \"{0}\"", remoteUri);
         }
     }
 }
